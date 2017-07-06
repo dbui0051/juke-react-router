41fca76c6e69987d6883b63764b453f39f7706e3
Jing Jia <nycjing@gmail.com>
2017-07-06 10:59:26 -0400

    modify the main.js

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
 import AllArtists from './AllArtists';
 import SingleArtist from './SingleArtist';
+import StatefulAlbums from './StatefulAlbums'
 import { HashRouter, Route } from 'react-router-dom';
 
 export default class Main extends Component {
@@ -17,8 +17,8 @@ export default class Main extends Component {
             <Sidebar />
           </div>
             <div className="col-xs-10">
-              <Route exact path="/" component={AllAlbums} />
-              <Route exact path="/albums" component={AllAlbums} />
+              <Route exact path="/" component={Albums} />
+              <Route exact path="/albums" component={StatefulAlbums} />
               <Route path="/albums/:albumId" component={SingleAlbum} />
               <Route exact path="/artists" component={AllArtists} /> 
               <Route path="/artists/:artistId" component={SingleArtist} />    
